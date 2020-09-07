7d7ea67132b899c7ac05b42b81e4d62924121fc6
Vladislav <vladprofmet@gmail.com>
2020-09-07 12:36:28 +0300

    fixed mail

--- mail.php
+++ mail.php
@@ -1,22 +1,42 @@
 <?php
-// Email Submit
-// Note: filter_var() requires PHP >= 5.2.0
-if ( isset($_POST['email']) && isset($_POST['name'])  && isset($_POST['message'])) {
- 
-  // detect & prevent header injections
-  $test = "/(content-type|bcc:|cc:|to:)/i";
-  foreach ( $_POST as $key => $val ) {
-    if ( preg_match( $test, $val ) ) {
-      exit;
+$method = $_SERVER['REQUEST_METHOD'];
+//Script Foreach
+$c = true;
+if ( $method === 'POST' ) {
+    $project_name = trim($_POST["project_name"]);
+    $admin_email  = trim($_POST["admin_email"]);
+    $form_subject = trim($_POST["form_subject"]);
+    foreach ( $_POST as $key => $value ) {
+        if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+            $message .= "
+			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+			</tr>
+			";
+        }
     }
-  }
-$to  = "<mail@example.com>" ; 
-$to .= "mail2@example.com>"; 
- 
-mail( $to, $_POST['name'], $_POST['message'], "From:" . $_POST['email'] );
-
-  
-  //			^
-  //  Replace with your email 
+} else if ( $method === 'GET' ) {
+    $project_name = trim($_GET["project_name"]);
+    $admin_email  = trim($_GET["admin_email"]);
+    $form_subject = trim($_GET["form_subject"]);
+    foreach ( $_GET as $key => $value ) {
+        if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+            $message .= "
+			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+			</tr>
+			";
+        }
+    }
 }
-?>
+$message = "<table style='width: 100%;'>$message</table>";
+function adopt($text) {
+    return '=?UTF-8?B?'.Base64_encode($text).'?=';
+}
+$headers = "MIME-Version: 1.0" . PHP_EOL .
+    "Content-Type: text/html; charset=utf-8" . PHP_EOL .
+    'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
+    'Reply-To: '.$admin_email.'' . PHP_EOL;
+mail($admin_email, adopt($form_subject), $message, $headers );
